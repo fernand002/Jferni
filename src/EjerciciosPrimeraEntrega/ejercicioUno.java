bab6e17861a4703faecfc794059b26128878eb99
PC GAMING <PC GAMING@F>
2024-10-02 22:07:41 +0200

    ejercicioUno

---
+++ src/EjerciciosPrimeraEntrega/ejercicioUno.java
@@ -0,0 +1,14 @@
+package EjerciciosPrimeraEntrega;
+
+public class ejercicioUno {
+    public static void main(String[] args) {
+        int entero = 10;
+        double decimal = 15.5;
+        String cadena = "Hola Mundo";
+
+        System.out.println("Entero: " + entero);
+        System.out.println("Decimal: " + decimal);
+        System.out.println("Cadena: " + cadena);
+  }
+}
+
