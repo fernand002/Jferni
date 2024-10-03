d8e0e681ae21935630e6efca15bcfa5a68614c71
PC GAMING <PC GAMING@F>
2024-10-03 21:19:57 +0200

    segundaentrega

--- src/EjerciciosPrimeraEntrega/ejercicioCuatro.java
+++ src/EjerciciosPrimeraEntrega/ejercicioCuatro.java
@@ -2,7 +2,7 @@ package EjerciciosPrimeraEntrega;
 
 public class ejercicioCuatro {
 	    public static void main(String[] args) {
-	        double celsius = 25;
+	        int celsius = 25;
 	        double fahrenheit = (celsius * 9/5) + 32;
 
 	        System.out.println("La temperatura en Fahrenheit es: " + fahrenheit);
