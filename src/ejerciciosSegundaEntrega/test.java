599ca014c597073f2fb8d6ea3fd286736e6ff869
PC GAMING <PC GAMING@F>
2024-10-03 21:22:48 +0200

    test

---
+++ src/ejerciciosSegundaEntrega/test.java
@@ -0,0 +1,19 @@
+package ejerciciosSegundaEntrega;
+
+public class test {
+	public class ejercicioSiete {
+		public static void main(String[]args) {
+			
+			int varNum =  5;
+			int varNum1 = 12 ; 
+			int varNum2 = 14 ; 
+			
+			double promedio = (varNum + varNum1 + varNum2) / 3.0 ;
+			
+			System.out.println("El promedio es: " + promedio);
+	    }
+			
+			
+		}
+
+}
